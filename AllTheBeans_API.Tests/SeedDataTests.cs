cb26ea9f9e1adb0d8d4ab8024e2481cb1eb600f5
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-06-17 19:51:38 +0100

    test: SeedData form json

--- AllTheBeans_API.Tests/SeedDataTests.cs
+++ AllTheBeans_API.Tests/SeedDataTests.cs
@@ -9,7 +9,7 @@ namespace AllTheBeans_API.Tests;
 public class SeedDataTests
 {
     [Fact]
-    public async Task Test1()
+    public async Task SeedData_Initialize_WithValidJson_SeedsCoffeesCorrectly()
     {
         //I decided to use a guid for the database name so that it stays unique between each test run
         var options = new DbContextOptionsBuilder<CoffeeDbContext>()
@@ -18,9 +18,23 @@ public class SeedDataTests
         
         var serviceProvider = new ServiceCollection()
             .AddSingleton(options)
-            .AddDbContext<CoffeeDbContext>()
             .BuildServiceProvider();
+        
+        // var testJson = await File.ReadAllTextAsync("Data.coffees.json");
+        var testProjectDir = Directory.GetCurrentDirectory();
+        var jsonFilePath = Path.Combine(testProjectDir, "Data", "coffees.json");
+        var testJson = await File.ReadAllTextAsync(jsonFilePath);
+        
+        //Act
+        await SeedData.Initialize(serviceProvider, testJson);
+        
+        //Aserts
+        await using var ctx = new CoffeeDbContext(options);
+        var coffees = await ctx.Coffees.ToListAsync();
 
-        // await SeedData.Initialize(serviceProvider);
+        Assert.NotEmpty(coffees);
+        Assert.All(coffees, coffee => Assert.NotNull(coffee.Name));
+        Assert.All(coffees, coffee => Assert.NotNull(coffee.Image));
+        Assert.All(coffees, coffee => Assert.IsType<decimal>(coffee.Cost));
     }
 }
